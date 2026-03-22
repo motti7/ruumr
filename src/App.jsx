1b79d837bd6e8b36bf69dbc07e7e60edb3997daf
מוטי שיפר <mottishif7@gmail.com>
2026-03-22 16:31:58 +0000

    File changes

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import PageNotFound from './lib/PageNotFound';
 import { AuthProvider, useAuth } from '@/lib/AuthContext';
 import UserNotRegisteredError from '@/components/UserNotRegisteredError';
+import GroupTracker from './pages/GroupTracker';
 
 const { Pages, Layout, mainPage } = pagesConfig;
 const mainPageKey = mainPage ?? Object.keys(Pages)[0];
@@ -60,6 +61,7 @@ const AuthenticatedApp = () => {
           }
         />
       ))}
+      <Route path="/GroupTracker" element={<LayoutWrapper currentPageName="GroupTracker"><GroupTracker /></LayoutWrapper>} />
       <Route path="*" element={<PageNotFound />} />
     </Routes>
   );
