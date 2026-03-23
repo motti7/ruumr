5343b9efb2ad71c68776f41114ea3b137b4e071b
מוטי שיפר <mottishif7@gmail.com>
2026-03-23 11:51:54 +0000

    File changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import './App.css'
+import { useState } from 'react'
 import { Toaster } from "@/components/ui/toaster"
+import SplashScreen from '@/components/SplashScreen'
+import { AnimatePresence } from 'framer-motion'
 import { QueryClientProvider } from '@tanstack/react-query'
 import { queryClientInstance } from '@/lib/query-client'
 import VisualEditAgent from '@/lib/VisualEditAgent'
