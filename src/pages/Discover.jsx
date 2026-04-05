147fdb1f95abc06ce553837b60bd688a3f7259b5
מוטי שיפר <mottishif7@gmail.com>
2026-04-05 18:37:45 +0000

    File changes

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -316,7 +316,7 @@ export default function DiscoverPage() {
                       className={`absolute inset-0 transition-all duration-300 ${
                         isTopCard
                           ? 'z-10 scale-100 translate-y-0 opacity-100'
-                          : '-z-10 scale-95 translate-y-12 opacity-80'
+                          : '-z-10 scale-95 translate-y-8 opacity-80'
                       }`}
                     >
                       <ProfileCard
