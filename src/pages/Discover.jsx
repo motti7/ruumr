fb0d06c95208fc1160878ea6c19db0882f175520
מוטי שיפר <mottishif7@gmail.com>
2026-04-05 19:40:24 +0000

    צמצום מרווח כרטיס תחתון

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -304,7 +304,7 @@ export default function DiscoverPage() {
 
       <DiscoverFilters filters={filters} onChange={applyFilters} />
 
-      <div className="absolute w-full flex items-start justify-center px-3" style={{ paddingTop: 'calc(env(safe-area-inset-top) + 8px)' }}>
+      <div className="absolute w-full flex items-start justify-center px-3" style={{ paddingTop: 'calc(env(safe-area-inset-top) + 56px)' }}>
         <div style={{ height: 'calc(100vh - 180px)', width: '100%', maxWidth: '448px', position: 'relative' }}>
           <AnimatePresence mode="wait">
             {hasProfiles ? (
